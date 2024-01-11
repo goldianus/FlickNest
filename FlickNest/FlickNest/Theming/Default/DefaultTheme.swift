e6b49652d2a1e99d391f4d88b200b6fad51a2702
Goldy <goldianussm@gmail.com>
2024-01-11 17:17:23 +0700

    feat(): View of the tab bar

---
+++ FlickNest/FlickNest/Theming/Default/DefaultTheme.swift
@@ -0,0 +1,11 @@
+//
+//  DefaultTheme.swift
+//  FlickNest
+//
+//  Created by Goldianus Solangius on 13/01/24.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ FlickNest/FlickNest/View/HomeScreen/MainBanner.swift
@@ -0,0 +1,18 @@
+//
+//  MainBanner.swift
+//  FlickNest
+//
+//  Created by Goldianus Solangius on 13/01/24.
+//
+
+import SwiftUI
+
+struct MainBanner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainBanner()
+}
